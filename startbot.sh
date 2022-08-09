66fe97416a1ce942efb45d321a6a204d4bc78fc8
kiwvic <torogao@protonmail.com>
2022-08-09 18:53:58 +0300

    add start script

---
+++ startbot.sh
@@ -0,0 +1,8 @@
+yarn make-market \
+    --network=mainnet \
+    --nearAccountId=$YOUR_ACCOUNT_ID \
+    --tonicContractId=v1.orderbook.near \
+    --marketId=$MARKET_ID \
+    --assetName=pem \
+    --baseQuantity=1 \
+    --orderDelayMs 60000
