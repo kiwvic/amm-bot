be962cf63df48fd56176a6088518cdef2102fb53
kiwvic <torogao@protonmail.com>
2022-08-10 17:46:25 +0300

    fix constants

--- src/consts.ts
+++ src/consts.ts
@@ -6,7 +6,7 @@ dotenv.config();
 export const CONFIG_URL = process.env.CONFIG_URL;
 
 const TOKEN_DECIMALS = 18;
-const PRICE_DECIMALS = 7;
+const PRICE_DECIMALS = 6;
 
-export const QUANTITY_FACTOR = Math.pow(10, TOKEN_DECIMALS - 1);
-export const PRICE_FACTOR = Math.pow(10, PRICE_DECIMALS - 1);
+export const QUANTITY_FACTOR = Math.pow(10, TOKEN_DECIMALS);
+export const PRICE_FACTOR = Math.pow(10, PRICE_DECIMALS);
