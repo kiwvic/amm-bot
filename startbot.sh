f7cadb02c6ffe3563081e351723230a9dda95373
kiwvic <torogao@protonmail.com>
2022-08-12 18:28:18 +0300

    add real data to startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -4,6 +4,6 @@ yarn make-market \
     --tonicContractId=v1.orderbook.near \
     --marketId=$MARKET_ID \
     --assetName=token.pembrock.near \
-    --baseQuantityPEM=10 \
-    --baseQuantityUSDC=3 \
-    --orderDelayMs 60000
+    --baseQuantityPEM=10000 \
+    --baseQuantityUSDC=1400 \
+    --orderDelayMs 120000
